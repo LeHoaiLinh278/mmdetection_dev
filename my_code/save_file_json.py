14f935cc72dbe47bdf9930e61fd137013671bf7f
LeHoaiLinh278 <linhspkt2708@gmail.com>
2020-02-16 12:07:42 +0000

    update file save_json

--- my_code/save_file_json.py
+++ my_code/save_file_json.py
@@ -4,8 +4,7 @@ import numpy as np
 import pycocotools.mask as maskUtils
 from mmcv.image import imread, imwrite
 import mmcv
-from imantics import Polygons, Mask
-from shapely.geometry import Polygon
+from imantics import Polygons, Mask  #(pip install imantics)
 
 def save_json(image, bboxes, segm_result, labels, class_names, score_thr=0, out_file=None):
     assert bboxes.ndim == 2
@@ -14,6 +13,10 @@ def save_json(image, bboxes, segm_result, labels, class_names, score_thr=0, out_
     assert bboxes.shape[1] == 4 or bboxes.shape[1] == 5
     img = image.copy()
 
+    if segm_result is not None:
+        segms = mmcv.concat_list(segm_result)
+        indxs = np.where(bboxes[:, -1] > score_thr)[0]
+
     if score_thr > 0:
         assert bboxes.shape[1] == 5
         scores = bboxes[:, -1]
@@ -21,17 +24,14 @@ def save_json(image, bboxes, segm_result, labels, class_names, score_thr=0, out_
         bboxes = bboxes[inds, :]
         labels = labels[inds]
 
-    if segm_result is not None:
-        segms = mmcv.concat_list(segm_result)
-
     record = {}
     record['imgHeight'] = img.shape[0]
     record['imgWidth'] = img.shape[1]
     objects = []
     if out_file is not None:
-        for i, (bbox, label, segm) in enumerate(zip(bboxes, labels, segms)):
+        for i, (bbox, label, indx) in enumerate(zip(bboxes, labels, indxs)):
             bbox_int = bbox.astype(np.int32)
-            mask = maskUtils.decode(segms[i]).astype(np.bool)
+            mask = maskUtils.decode(segms[indx]).astype(np.bool)
             polygons = Mask(mask).polygons().segmentation
             label_text = class_names[
                 label] if class_names is not None else 'cls {}'.format(label)
