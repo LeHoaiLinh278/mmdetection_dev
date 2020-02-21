d46592aa0af357a1a61bf5afdaf6add393fa1b55
LeHoaiLinh278 <linhspkt2708@gmail.com>
2020-02-21 04:16:23 +0000

    add efficientnet

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -1,6 +1,7 @@
+from .efficientnet import EfficientNet
 from .hrnet import HRNet
 from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'EfficientNet']
